b7fb40338892ac9f33d2c3a598b8eb1d131790e5
nickhillwd <nickhillwd@gmail.com>
2016-03-01 13:36:21 +0000

    log in redering profile page

--- app/routes.js
+++ app/routes.js
@@ -43,6 +43,14 @@ module.exports = function(app, passport) {
     // =====================================
     // PROFILE SECTION =====================
     // =====================================
+
+    // process the login form
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/login', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
+        
     // we will want this protected so you have to be logged in to visit
     // we will use route middleware to verify this (the isLoggedIn function)
     app.get('/profile', isLoggedIn, function(req, res) {
